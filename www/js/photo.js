174d63dfc09b43010b8313c54712b800551db0e0
Travis Neilans <travis@airhorndesign.com>
2013-03-14 12:25:04 -0400

    dfgsd

--- www/js/photo.js
+++ www/js/photo.js
@@ -4,49 +4,71 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	// Do cool things here...
 }
-function getImage() {
-// Retrieve image file location from specified source
-navigator.camera.getPicture(uploadPhoto, function(message) {
-	alert('get picture failed');
-	},{
-	quality: 20,
-	destinationType: navigator.camera.DestinationType.FILE_URI,
-	sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY,
-	encodingType: navigator.camera.EncodingType.JPEG
-	}
-	);
-}
-function takePic() {
-// Retrieve image file location from specified source
-navigator.camera.getPicture(uploadPhoto, function(message) {
-	alert('get picture failed');
-	},{
-	quality: 20,
-	destinationType: navigator.camera.DestinationType.FILE_URI,
-	sourceType: navigator.camera.PictureSourceType.CAMERA,
-	allowEdit : true,
-	encodingType: navigator.camera.EncodingType.JPEG
-	}
-	);
-}
-function uploadPhoto(imageURI) {
-	var options = new FileUploadOptions();
-	options.fileKey="file";
-	options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1)+'.jpg';
-	options.mimeType="image/jpeg";
-	var params = new Object();
+function takePicture() {
+        navigator.camera.getPicture(
+            function(uri) {
+                var img = document.getElementById('camera_image');
+                img.style.visibility = "visible";
+                img.style.display = "block";
+                img.src = uri;
+                document.getElementById('camera_status').innerHTML = "Success";
+            },
+            function(e) {
+                console.log("Error getting picture: " + e);
+                document.getElementById('camera_status').innerHTML = "Error getting picture.";
+            },
+            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI});
+    };
 
-	options.params = params;
-	options.chunkedMode = false;
-	var ft = new FileTransfer();
-	ft.upload(imageURI, encodeURI("http://airhorndesign.com/m_igotdibs/upload.php"), win, fail, options);
-}
-function win(r) {
-	console.log("Code = " + r.responseCode);
-	console.log("Response = " + r.response);
-	console.log("Sent = " + r.bytesSent);
-	alert(r.response);
-}
-function fail(error) {
-	alert("An error has occurred: Code = " + error.code);
-}
+    /**
+* Select picture from library
+*/
+    function selectPicture() {
+        navigator.camera.getPicture(
+            function(uri) {
+                var img = document.getElementById('camera_image');
+                img.style.visibility = "visible";
+                img.style.display = "block";
+                img.src = uri;
+                document.getElementById('camera_status').innerHTML = "Success";
+            },
+            function(e) {
+                console.log("Error getting picture: " + e);
+                document.getElementById('camera_status').innerHTML = "Error getting picture.";
+            },
+            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY});
+    };
+    
+    /**
+* Upload current picture
+*/
+    function uploadPicture() {
+    
+     // Get URI of picture to upload
+        var img = document.getElementById('camera_image');
+        var imageURI = img.src;
+        if (!imageURI || (img.style.display == "none")) {
+            document.getElementById('camera_status').innerHTML = "Take picture or select picture from library first.";
+            return;
+        }
+        
+        // Verify server has been entered
+        server = "http://airhorndesign.com/m_igotdibs/upload.php";
+        if (server) {
+        
+            // Specify transfer options
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1)+'.jpg';
+            options.mimeType="image/jpeg";
+            options.chunkedMode = false;
+
+            // Transfer picture to server
+            var ft = new FileTransfer();
+            ft.upload(imageURI, server, function(r) {
+                document.getElementById('camera_status').innerHTML = "Upload successful: "+r.bytesSent+" bytes uploaded.";
+            }, function(error) {
+                document.getElementById('camera_status').innerHTML = "Upload failed: Code = "+error.code;
+            }, options);
+        }
+    }
