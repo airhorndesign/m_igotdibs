7c5bc1181a082a9d3fe90d6f6e1c640161036360
Travis Neilans <travis@airhorndesign.com>
2013-03-14 13:33:35 -0400

    zdbdsabadfbdb

--- www/js/photo.js
+++ www/js/photo.js
@@ -37,7 +37,7 @@ function takePicture() {
                 console.log("Error getting picture: " + e);
                 document.getElementById('camera_status').innerHTML = "Error getting picture.";
             },
-            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY,encodingType: Camera.EncodingType.JPEG});
+            { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.SAVEDPHOTOALBUM,encodingType: Camera.EncodingType.JPEG});
     };
     
     /**
